29fc3d98868edcf501156cbd9a4c2b2b01aabfdc
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2020-12-07 03:19:09 +0300

    Init repo commit

---
+++ api/index.ts
@@ -0,0 +1,41 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+import { cardRender } from '../utils/card';
+import toBase64ImageUrl from "../utils/toBase64"
+
+export default async function render(req: NowRequest, res: NowResponse) {
+    try {
+        const { name, description, image, backgroundColor, iconColor, site, fontColor, pattern, opacity, colorPattern, instagram, linkedin, github, twitter  } = req.query;
+        
+        const _linkedin = linkedin ?? ''
+        const _instagram = instagram ?? ''
+        const _twitter = twitter ?? ''
+        const _github = github ?? ''
+        const _site = site ?? ''
+        const _image = await toBase64ImageUrl(image)
+
+        const card = cardRender({
+            name,
+            description,
+            iconColor,
+            backgroundColor,
+            _image,
+            pattern,
+            colorPattern,
+            _site,
+            fontColor,
+            opacity,
+            _instagram,
+            _linkedin,
+            _github,
+            _twitter,
+        });
+
+        res.setHeader('Content-type', 'image/svg+xml');
+        return res.send(card);
+    } catch (e) {
+        return res.send({
+            status: 'err',
+            message: e.message
+        });
+    }
+}
---
+++ typings/types.ts
@@ -0,0 +1,41 @@
+export enum HeroPattern {
+    plus = 'plus',
+    topography = 'topography',
+    texture = 'texture',
+    hideout = 'hideout',
+    fallingTriangles = 'fallingTriangles',
+    iLikeFood = 'iLikeFood',
+    fourPointStars = 'fourPointStars',
+    brickWall = 'brickWall',
+    wiggle = 'wiggle',
+    bubbles = 'bubbles',
+    floatingCogs = 'floatingCogs',
+    leaf = 'leaf',
+    rain = 'rain',
+    polkaDots = 'polkaDots',
+    ticTacToe = 'ticTacToe'
+}
+
+export interface Icons {
+    github?: string
+    instagram?: string
+    linkedin?: string
+    twitter?: string
+}
+
+export interface ParsedRequest {
+    name: string | string[]
+    description: string | string[]
+    _image: string | string[]
+    colorPattern: string | string[]
+    fontColor?: string | string[]
+    iconColor?: string | string[]
+    backgroundColor?: string | string[]
+    _site?: string | string[]
+    pattern?: string | string[]
+    opacity?: string | string[]
+    _instagram?: string | string[]
+    _linkedin?: string | string[]
+    _github?: string | string[]
+    _twitter?: string | string[]
+}
